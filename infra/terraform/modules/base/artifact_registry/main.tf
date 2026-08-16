5f5a074307acc35932744f8702870ea078e16718
Yvonne Yao <yvonne.yao278@gmail.com>
2026-08-16 20:53:10 +1000

    fix(ST-21): satisfy Terraform lint baseline

--- infra/terraform/modules/base/artifact_registry/main.tf
+++ infra/terraform/modules/base/artifact_registry/main.tf
@@ -1,3 +1,8 @@
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers { google = { source = "hashicorp/google", version = "~> 6.0" } }
+}
+
 resource "google_artifact_registry_repository" "this" {
   project       = var.project_id
   location      = var.location
