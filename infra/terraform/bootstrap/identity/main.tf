9968124d79ac00381ea532e3897553b4329635d5
Yvonne Yao <yvonne.yao278@gmail.com>
2026-08-16 21:17:26 +1000

    infra(ST-23): isolate Terraform bootstrap roots

---
+++ infra/terraform/bootstrap/identity/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.9.0"
+  backend "gcs" {}
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+locals {
+  environment = jsondecode(file(var.environment_config))
+  project     = jsondecode(file(var.project_config))
+}
+
+provider "google" { project = local.project.project_id }
+
+module "bootstrap_identity" {
+  source = "../../modules/foundations/bootstrap_identity"
+
+  project_id         = local.project.project_id
+  github_owner       = split("/", local.project.github_repository)[0]
+  github_repo        = split("/", local.project.github_repository)[1]
+  allowed_ref        = "refs/heads/main"
+  environment        = local.environment.environment
+  service_account_id = "github-terraform-sandbox"
+  project_roles      = var.project_roles
+}
